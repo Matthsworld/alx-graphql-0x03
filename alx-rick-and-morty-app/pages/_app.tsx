74d69588ac01f98ad7c8252364da9fdd4f08cf05
Matthsworld <Matthsworld@gmail.com>
2025-01-30 17:57:16 +0100

    update: modified _apps.tsx

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -3,6 +3,20 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/apolloClient";
 
+import ErrorBoundary from '@/components/ErrorBoundary';
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
